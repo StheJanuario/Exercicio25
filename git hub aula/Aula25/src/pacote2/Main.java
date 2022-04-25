58bfaec1d3bf410ec75d6f91a34f457b0296fd88
StheJanuario <103960533+StheJanuario@users.noreply.github.com>
2022-04-25 14:33:58 -0300

    Add files via upload

---
+++ git hub aula/Aula25/src/pacote2/Main.java
@@ -0,0 +1,17 @@
+package pacote2;
+import java.io.*;
+import java.nio.file.*;
+import java.util.stream.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String file = "arquivo.txt";
+        try (Stream<String> stream = Files.lines(Paths.get(file))) {
+            stream.forEach(System.out::println);
+        }
+        catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ git hub aula/Aula25/src/pacote3/Main.java
@@ -0,0 +1,15 @@
+package pacote3;
+ import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String [] args) throws IOException {
+        String fileName = "arquivo.txt";
+        Scanner scan = new Scanner(new File(fileName));
+        while(scan.hasNextLine()){
+            String line = scan.nextLine();
+            System.out.println(line);
+        }
+    }
+}
+
